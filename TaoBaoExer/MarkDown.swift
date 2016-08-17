9b8a18ca25489979afd3764a6bf8531fbf1d375c
NancyZY <980395669@qq.com>
2016-08-17 15:43:04 +0800

    Show UIMenuController from UIBarButtonItem

--- TaoBaoExer/MarkDown.swift
+++ TaoBaoExer/MarkDown.swift
@@ -16,13 +16,13 @@ class MarkDown: UIViewController,UITextViewDelegate {
     @IBOutlet weak var originText: UITextView!
     
     
-    var trashItem : UIBarButtonItem!
+    var undoItem : UIBarButtonItem!
     
     var flexableSpaceItem : UIBarButtonItem {
         return UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
     }
     
-    var doneSpaceItem : UIBarButtonItem!
+    var redoItem : UIBarButtonItem!
     
     var secondFlexableSpaceItem : UIBarButtonItem {
         return UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
@@ -35,14 +35,14 @@ class MarkDown: UIViewController,UITextViewDelegate {
         originText.text = "enter something..."
         originText.delegate = self
         
-        trashItem = UIBarButtonItem(barButtonSystemItem: .Trash, target: self, action: "clickCenter:")
+        undoItem = UIBarButtonItem(image:UIImage(named:"undo"), style: .Done, target: self, action: "clickCenter:")
         
-        doneSpaceItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "clickCenter:")
+        redoItem = UIBarButtonItem(image:UIImage(named:"redo"), style: UIBarButtonItemStyle.Plain, target: self, action: "clickCenter:")
         
         geishaItem = UIBarButtonItem(image:UIImage(named:"geisha"), style: .Done, target: self, action: "clickCenter:")
         
-        trashItem.tag = 101
-        doneSpaceItem.tag = 102
+        undoItem.tag = 101
+        redoItem.tag = 102
         geishaItem.tag = 103
         
         configToolBar()
@@ -78,9 +78,9 @@ class MarkDown: UIViewController,UITextViewDelegate {
         let numberToolbar = UIToolbar(frame: CGRectMake(0, 0, self.view.frame.size.width, 50))
         numberToolbar.barStyle = UIBarStyle.Default
         numberToolbar.items = [
-            trashItem,
+            undoItem,
             flexableSpaceItem,
-            doneSpaceItem,
+            redoItem,
             secondFlexableSpaceItem,
             geishaItem
             ]
@@ -92,14 +92,17 @@ class MarkDown: UIViewController,UITextViewDelegate {
     {
         print (barItem.tag)
         switch barItem.tag {
-        case 1:
-            print ("GoodSomeThingClickType")
+        case 101:
+            // 加粗
+            originText.typingAttributes[NSObliquenessAttributeName] = (originText.typingAttributes[NSObliquenessAttributeName] as? NSNumber) == 0 ? 0.5 : 0
             break
-        case 2:
-           print("搜索")
+        case 102:
+            // 加下划线
+           originText.typingAttributes[NSUnderlineStyleAttributeName] =  (NSUnderlineStyle.StyleSingle.hashValue ) == 0 ? 1 : NSUnderlineStyle.StyleSingle.hashValue
             break
-        case 3:
-            print("种草")
+        case 103:
+            // 弹出 UIMenuController
+            quickJumpClicked(barItem)
             break
         case 4:
             print("签到")
@@ -107,5 +110,44 @@ class MarkDown: UIViewController,UITextViewDelegate {
         default :
             break
         }
+    }
+    
+    func quickJumpClicked(sender: AnyObject){
+        // 1. 成为第一响应者
+        self.becomeFirstResponder()
+        
+        // 2. 获取菜单
+        let menu = UIMenuController.sharedMenuController()
+        
+        // 3. 设置自定义菜单
+        menu.menuItems = [ UIMenuItem.init(title: "呵呵哒", action: Selector.init("heHeDa:")) ]
+        
+        // 4. 菜单显示位置
+        menu.setTargetRect(sender.view!.bounds, inView: self.view)
+        
+        // 5. 显示菜单
+        menu.setMenuVisible(true, animated: true)
+    }
+    
+    func heHeDa(menu :UIMenuController )
+    {
+        print("测试方法而已, 呵呵哒")
+    }
+    
+    //MARK: 让Lable具备成为第一响应者的资格
+    override func canBecomeFirstResponder() -> Bool
+    {
+        return true
+    }
+    
+    //MARK: 返回悬浮菜单中可以显示的选项
+    override func canPerformAction(action: Selector, withSender sender: AnyObject?) -> Bool
+    {
+        // 判断 action 中包含的各个事件的方法名称, 对比上了才能显示
+        if (action == Selector.init("copy:") || action == Selector.init("heHeDa:"))
+        {
+            return true
+        }
+        return false
     }
 }
--- TaoBaoExer/MoreUIBarButtonItem.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MoreUIBarButtonItem.swift
-//  TaoBaoExer
-//
-//  Created by Nancy's MacbookPro on 8/17/16.
-//  Copyright © 2016 Nancy's MacbookPro. All rights reserved.
-//
-
-import Foundation
